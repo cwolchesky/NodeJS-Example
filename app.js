d1b159ba91214eee5718fa6aead7ad8a73e9f7ab
Chris Wolchesky <chris@wolchesky.net>
2015-04-14 18:53:25 -0400

    Split route functions off into router.js
    Created user route.  Removed 501 return.

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-
+var router = require("./router.js");
 
 // Start a web server
 var http = require('http');
 http.createServer(function (request, response) {
-	homeRoute(request, response);
+	router.home(request, response);
+	router.user(request, response);
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337');
 
-function homeRoute(request, response) {
-	//if url == "/" && GET
-	if (request.url === "/") {
-		//show search
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write("Header\n");
-		response.write("Search\n");
-		response.end('Footer\n');
-	} else {
-		response.writeHead(501);
-		response.end();
-	}
-	//if url == "/" && POST
-	  //redirect to /:username
-}
