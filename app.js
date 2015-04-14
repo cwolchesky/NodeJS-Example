c930d1afe5679168f1ff81cd2a0b007ed7e71c78
Chris Wolchesky <chris@wolchesky.net>
2015-04-13 23:19:15 -0400

    Started building out home route
    Added 501 response for user routes.

--- app.js
+++ app.js
@@ -3,10 +3,22 @@
 // Start a web server
 var http = require('http');
 http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	setInterval(function() {
-		response.write(new Date() + "\n");
-	}, 1000);
-	//response.end('Hello World\n');
+	homeRoute(request, response);
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337');
+
+function homeRoute(request, response) {
+	//if url == "/" && GET
+	if (request.url === "/") {
+		//show search
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write("Header\n");
+		response.write("Search\n");
+		response.end('Footer\n');
+	} else {
+		response.writeHead(501);
+		response.end();
+	}
+	//if url == "/" && POST
+	  //redirect to /:username
+}
