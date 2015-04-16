360245c291c2924748875fd1432038a57abb91a0
Chris Wolchesky <chris@wolchesky.net>
2015-04-15 22:27:10 -0400

    Fixed headers, settting up POST handling

--- router.js
+++ router.js
@@ -1,25 +1,42 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var querystring = require("querystring");
+
+var commonHeaders = {'Content-Type': 'text/html'};
 
 function home(request, response) {
 	//if url == "/" && GET
 	if (request.url === "/") {
-		//show search
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		renderer.view("header", {}, response);
-		renderer.view("search", {}, response);
-		renderer.view("footer", {}, response);
-		response.end();
+		if (request.method.toLowerCase() === "get") {
+			//show search
+			response.writeHead(200, commonHeaders);
+			renderer.view("header", {}, response);
+			renderer.view("search", {}, response);
+			renderer.view("footer", {}, response);
+			response.end();
+		} else {
+			//if url == "/" && POST
+			
+			//get the post data from body
+			request.on("data", function(postBody) {
+				console.log(postBody.toString());
+				//extract username
+				var query = querystring.parse(postBody.toString());
+				//redirect to /:username
+				response.writeHead(303, {"Location": "/" + query.username});
+				response.end();
+			});
+			
+		}
 	}
-	//if url == "/" && POST
-	  //redirect to /:username
+	
 }
 
 function user(request, response) {
 	//if url == "/...."
 	var username = request.url.replace("/","");
 	if(username.length > 0) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.writeHead(200, commonHeaders);
 		renderer.view("header", {}, response);
 		//get json from Treehouse
 		var studentProfile = new Profile(username);
